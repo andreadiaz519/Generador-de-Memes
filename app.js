e3374546a07bc84ebccd70e7dd216545f99fd616
andreadiaz519 <andreadiaz_2308@hotmail.com>
2024-07-04 10:58:10 -0500

    Avances con Java

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-const botonDescarga = document.getElementById(`button-descarga`);
-const contenedorMeme = document.getElementById(`contenedor-principal`);
+
+// // // boton checkbox
+// const checkbox = document.getElementById("checkbox");
+// checkbox.addEventListener("chage", function() {
+// console.log("Cambio el estado del checkbox");
+// }
+// )
+
+// // function mostrar() {
+// //     document.getElementById(`texto-meme`).style.display = `block`;
+// // }
+
+
+
+
+
+
+
+
+
+// Boton Descarga
+const botonDescarga = document.getElementById("button-descarga");
+const contenedorMeme = document.getElementById("contenedor-principal");
 
 function decargarMeme () {
-    console.log(domtoimage);
-    domtoimage.toBlob(contenedorMeme).then(function (blob) {
-        console.log(blob);
-    }
-    )
+console.log(domtoimage);
+domtoimage.toBlob(contenedorMeme).then(function (blob) {
 
-}
+saveAs(blob, `mi-meme.png`);
+})}
+
 botonDescarga.addEventListener(`click`, decargarMeme);
