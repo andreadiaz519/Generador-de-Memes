08ceb5fd73b57231c8cb09d08cb22394953b4859
andreadiaz519 <andreadiaz_2308@hotmail.com>
2024-07-06 01:03:08 -0500

    Avances del Generador

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
+// Modo claro-oscuro
+
+const darkMode = document.getElementById(`dark-mode`);
+const lightMode = document.getElementById(`light-mode`);
+const body = document.getElementById(`body`);
+
+function darkModeOn () {
+    body.classList.add(`dark-mode`);
+    body.classList.remove(`light-mode`);
+    darkMode.classList.add(`hidden`);
+    lightMode.classList.remove(`hidden`);
+}
+
+darkMode.addEventListener(`clik`, darkModeOn);
+
+function lightModeOn () {
+    body.classList.add(`dark-mode`);
+    body.classList.remove(`light-mode`);
+    darkMode.classList.add(`hidden`);
+    lightMode.classList.remove(`hidden`);
+}
+
+lightModeMode.addEventListener(`clik`, lightModeOn);
+
+
+
+
+
+
 
 // // // boton checkbox
 // const checkbox = document.getElementById("checkbox");
@@ -11,22 +40,56 @@
 // // }
 
 
-
-
-
-
-
-
-
 // Boton Descarga
-const botonDescarga = document.getElementById("button-descarga");
-const contenedorMeme = document.getElementById("contenedor-principal");
+const botonDescarga = document.getElementById(`button-descarga`);
+const contenedorMeme = document.getElementById(`contenedor-principal`);
 
 function decargarMeme () {
-console.log(domtoimage);
 domtoimage.toBlob(contenedorMeme).then(function (blob) {
-
-saveAs(blob, `mi-meme.png`);
-})}
+    console.log(blob);
+    saveAs(blob, `mi-meme.png`)
+}
+)}
 
 botonDescarga.addEventListener(`click`, decargarMeme);
+
+
+// Aside de Texto
+
+// Fuentes de texto
+// const titulo = document.getElementById(`fuente`);
+// const input = document.getElementById(`tipo-fuente`);
+// const select = document.getElementById(`font`);
+
+// // titulo.innerHTML = `Texto Default`;
+
+// input.addEventListenner(`input`, (e) => {
+// })
+
+// select.addEventListenner(`input`, (e) => {
+//     // console.log(e.target.value)
+//     titulo.style.fontFamily = e.target.value;
+
+// })
+
+
+// Aside de Imagen
+
+// Color de fondo
+
+
+
+
+
+// Fondo
+
+// const titulo = document.getElementById(fondo-de-imagen);
+// const button = document.getElementById(fondo-1);
+
+
+// titulo.innerHTML = `Texto Default`;
+
+// button.addEventListenner(`button`, (a) => {
+// titulo.style.alignItems = a.target.value;
+// })
+
